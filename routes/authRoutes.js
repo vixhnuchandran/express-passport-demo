07b5d118ff196ba669cc3b1e3b44b95325127e6c
vixhnuchandran <vixhnuchandran@gmail.com>
2023-12-04 06:38:09 +0530

    Set up a module to define authentication routes using Passport.js.

---
+++ routes/authRoutes.js
@@ -0,0 +1,40 @@
+const router = require("express").Router()
+const passport = require("passport")
+
+// auth login
+router.get("/login", (req, res) => {
+  return res.render("login")
+})
+
+// auth register
+router.get("/register", (req, res) => {
+  return res.render("register")
+})
+
+// auth logout
+router.get("/logout", (req, res) => {
+  req.logout
+  return res.redirect("/")
+})
+
+// auth with google
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+)
+// auth with github
+router.get("/github", passport.authenticate("github", { scope: ["profile"] }))
+
+// auth google callbackURI
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  // res.send(req.user)
+  return res.redirect("/profile")
+})
+
+// auth google callbackURI
+router.get("/github/redirect", passport.authenticate("github"), (req, res) => {
+  // res.send(req.user)
+  return res.redirect("/profile")
+})
+
+module.exports = router
